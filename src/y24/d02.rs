fe92e835e23a6b61abdbcf47dbd5ec65a25b0730
Sasank Madineni <sasank1020@gmail.com>
2024-12-03 11:39:54 -0800

    started working on d2p1

--- src/y24/d02.rs
+++ src/y24/d02.rs
@@ -3,6 +3,19 @@ use std::fs;
 const INPUT_FILE: &str = "input/y24/d02.txt";
 
 fn part_one() -> std::io::Result<u32> {
+    let binding = fs::read_to_string(INPUT_FILE).expect("File Read Error");
+    let content = binding.trim();
+    let content_vec = binding.split('\n').collect::<Vec<&str>>();
+    
+    let num_safe = 0;
+
+    for line in content_vec {
+        let l_vec = line.split(' ').collect::<Vec<&str>>();
+        for mut iter in l_vec.iter().enumerate() {
+            dbg!("{}", iter.0);
+        }
+    }
+    
 
     Ok(0)
 }
