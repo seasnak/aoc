6b490d0e4218a2b582234979d1943616db1e0d1e
Sasank Madineni <sasank1020@gmail.com>
2024-12-02 17:29:29 -0800

    smaller changes, didn't have time to fix the runner

--- src/y24/d01.rs
+++ src/y24/d01.rs
@@ -1,36 +1,46 @@
-use std::io;
-use std::env;
-use std::fs;
+//use std::io;
+//use std::env;
+//use std::fs;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
 
 const DAY: &str = "01";
 const INPUT_FILE: &str = "input/y24/d01.txt";
 
-fn part_one(reader:BufReader<File>) -> Result<i32,io::Error> {
+fn part_one(reader:BufReader<File>) -> std::io::Result<uszie> {
     let answer = 0;
     let content = reader.lines().flatten();
-    println!("{}", answer);
+        
     Ok(answer)
 
 }
 
-fn part_two(reader:BufReader<File>) -> Result<i32,io::Error> {
-    let answer = 0;
-    let content = reader.lines().flatten();
-    Ok(answer)
-}
+//fn part_two(reader:BufReader<File>) -> Result<usize> {
+//    let answer = 0;
+//    let content = reader.lines().flatten();
+//    Ok(answer)
+//}
 
-fn solve(part:i32=1) {
+pub fn solve(part:i32=1) -> std::io::Result<()> {
     if part == 1 {
         println!("=== PART 1 ===");
-        let f = File::open(INPUT_FILE);
-        println!("{:?}", part_one(BufReader::new(f)));
+        let f = File::open(INPUT_FILE)?;
+        let mut buf = BufReader::new(f);
+        println!("{}", part_one(buf));
     }
     else {
-        println!("=== PART 2 ===");
-        let f = File::open(INPUT_FILE);
-        println!("{:?}", part_two(BufReader::new(f)));
+        //println!("=== PART 2 ===");
+        //let f = File::open(INPUT_FILE)?;
+        //println!("{}", part_two(BufReader::new(f)));
     }
-    
+
+    Ok(())    
 }
+
+//fn main() {
+//    let args: Vec<String> = env::args().collect();
+//    assert_eq!(args.len(), 2);
+//
+//    solve(args[1].as_str().parse().unwrap());
+//
+//}
