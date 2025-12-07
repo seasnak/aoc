fbe54c3e3071d1afef80a3d675c99be89909af8a
Sasank Madineni <sasank1020@gmail.com>
2025-12-07 13:18:29 -0800

    working changes on d02

--- aoc-cpp/src/y25/d02.cpp
+++ aoc-cpp/src/y25/d02.cpp
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <sstream>
+#include <string>
 
 struct IDRange
 {
@@ -17,12 +18,40 @@ std::string readFileToString(const std::string& filename) {
     return bufferStream.str();
 }
 
-int getInvalidIDTotalFromRange(const IDRange idr)
+unsigned long getInvalidIDTotalFromRange(const IDRange idr)
 {
-    int total {0};
+    unsigned long total {0};
 
     // case: can't form repeated string from an odd number of characters
     if (idr.start.length() %2 != 0 && idr.end.length() == idr.start.length()) return 0;
+    else if (idr.end.length() == 1) return 0;
+
+    std::cout << "start: " << idr.start << " end: " << idr.end << std::endl;
+
+    unsigned long start_i = std::stoul(idr.start);
+    unsigned long end_i = std::stoul(idr.end);
+
+    std::string start_half {"0"};
+    if( idr.start.length() != 1)
+    {
+        start_half = idr.start.substr(0, idr.start.length() / 2);
+    }
+    std::string end_half { idr.end.substr(0, idr.end.length() / 2) };
+
+    unsigned long start_half_i { std::stoul(start_half) };
+    unsigned long end_half_i { std::stoul(end_half) };
+
+    for(unsigned long i {start_half_i}; i <= end_half_i; i++)
+    {
+        std::string current_id = std::to_string(i) + std::to_string(i);
+        std::cout << "current id: " << current_id << std::endl;
+        unsigned long current_id_i = std::stoul(current_id);
+
+        if (current_id_i > start_i && current_id_i < end_i)
+        {
+            total += current_id_i;
+        }
+    }
     
     return total;    
 }
@@ -33,7 +62,7 @@ int getInvalidIDTotal(const std::string& input_fname)
 
     std::stringstream ss(content);
     std::string token;
-    int total = 0;
+    unsigned long total = 0;
 
     IDRange id_range;
     
@@ -47,9 +76,10 @@ int getInvalidIDTotal(const std::string& input_fname)
             std::getline(id_ss, id_range.start, '-');
             std::getline(id_ss, id_range.end);
             // std::cout << "start: " << id_range.start << " end: " << id_range.end << std::endl;
+
+            total += getInvalidIDTotalFromRange(id_range);
         }
         
-        total += getInvalidIDTotalFromRange(id_range);
 
     }
 
@@ -60,7 +90,10 @@ int main() {
 
     std::string input_fname {"../../data/y25/d02.txt"};
 
-    getInvalidIDTotal(input_fname);
+    // PART ONE
+    std::cout << getInvalidIDTotal(input_fname) << std::endl;
+
+    // PART TWO
 
     return EXIT_SUCCESS;
 }
